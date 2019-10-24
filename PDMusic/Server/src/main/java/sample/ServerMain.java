31b7eee4abf3d2d2d59ffb9cebaed459cceb1b1d
Ricardo <33632491+rmcsilva@users.noreply.github.com>
2019-10-24 17:52:21 +0100

    Added MySql Connector/J Server Depedency
    Server will need Connector/J to connect to the MySql database

--- PDMusic/Server/src/main/java/sample/ServerMain.java
+++ PDMusic/Server/src/main/java/sample/ServerMain.java
@@ -1,7 +1,9 @@
 package sample;
 
+import sample.MySQLAcess;
+
 public class ServerMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Server main!");
     }
 }
