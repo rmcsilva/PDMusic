c9ca51309fc788f0564890021054bed2d69b1fe8
Ricardo <33632491+rmcsilva@users.noreply.github.com>
2019-11-13 23:10:42 +0000

    Update: Changed Communication back to being an interface
    The methods in communication will be implemented in both the client and the server in different ways hence the need for the comum interface.

--- PDMusic/Communication/src/sample/Communication.java
+++ PDMusic/Communication/src/sample/Communication.java
@@ -1,53 +1,13 @@
 package sample;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
 
-public class Communication {
-    private String ip = "127.0.0.1";
-    private int port = 8080;
-    private Socket socket;
+public interface Communication extends JSONConstants {
 
-    public void GenerateJSON(JSONObject options) throws IOException{
-        String jsonString = null;
-        try {
-            jsonString = options.toString();
-            System.out.println(jsonString);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+    void login(String username, String password) throws IOException;
+    void register(String name, String username,String password) throws IOException;
+    void addMusic(String name, String author, String album, int year, int duration, String genre);
+    void addPlaylist(String name);
+    void logout();
 
-        try{
-            socket = new Socket(ip, port);
-            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-            output.println(jsonString);
-        }catch(IOException io){
-            System.out.println("Error: " + io.getMessage());
-        }
-    }
-
-    public void ReadJSONFromServer(){
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
-            String jsonRec = bufferedReader.readLine();
-            JSONObject json = new JSONObject(jsonRec);
-
-            System.out.println(json.getString("tipo") +
-                    "\n" + json.getString("sucesso") +
-                    "\n" + json.getString("mensagem"));
-
-            printWriter.close();
-            bufferedReader.close();
-            socket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
