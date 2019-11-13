34833e244f980367114c491e481c0fd6dfa3c728
Ricardo <33632491+rmcsilva@users.noreply.github.com>
2019-11-13 23:16:17 +0000

    Added: Constants for the paths of the layouts
    All the paths of the layouts are no in the same place and used as constants

---
+++ PDMusic/Client/src/main/java/sample/controllers/LayoutsConstants.java
@@ -0,0 +1,19 @@
+package sample.controllers;
+
+public interface LayoutsConstants {
+    //Layout paths
+    String LAYOUT_LOGIN = "/layouts/login.fxml";
+    String LAYOUT_REGISTER = "/layouts/register.fxml";
+    String LAYOUT_MAIN_MENU = "/layouts/mainMenu.fxml";
+    //Layout tabs paths
+    String LAYOUT_TAB_ADD_MUSIC = "/layouts/musicsTab/addMusicMenu.fxml";
+    String LAYOUT_TAB_PLAYLIST_SELECTED = "/layouts/playlistsTab/playlistSelected.fxml";
+    String LAYOUT_TAB_ADD_PLAYLIST = "/layouts/playlistsTab/addPlaylistMenu.fxml";
+    String LAYOUT_TAB_SELECT_MUSICS = "/layouts/playlistsTab/selectMusics.fxml";
+    //Tabs name
+    String TAB_MUSICS = "Musics";
+    String TAB_PLAYLISTS = "Playlists";
+    //Tab sizes
+    double tabIconSize = 144.0;
+    int tabFontSize = 18;
+}
