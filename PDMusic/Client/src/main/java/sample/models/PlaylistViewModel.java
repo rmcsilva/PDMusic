e40219906ddf38f8ab513d4daa44e7e87f77ae7a
Ricardo <33632491+rmcsilva@users.noreply.github.com>
2019-11-14 18:07:43 +0000

    Added: PlaylistViewModel
    Model that will represent a playlist in the treeTableView

---
+++ PDMusic/Client/src/main/java/sample/models/PlaylistViewModel.java
@@ -0,0 +1,30 @@
+package sample.models;
+
+import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class PlaylistViewModel extends RecursiveTreeObject<PlaylistViewModel> {
+    private StringProperty name, username;
+
+    public PlaylistViewModel(String name, String username) {
+        this.name = new SimpleStringProperty(name);
+        this.username = new SimpleStringProperty(username);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public String getUsername() {
+        return username.get();
+    }
+
+    public StringProperty usernameProperty() {
+        return username;
+    }
+}
