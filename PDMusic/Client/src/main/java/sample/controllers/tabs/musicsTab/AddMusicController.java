d4928c45522dace80f167aacaf23c38ee3751b17
Ricardo <33632491+rmcsilva@users.noreply.github.com>
2019-11-14 13:05:28 +0000

    Added: Dynamic AddMusic and AddPlaylist
    If the forms are filled correctly the fields will be set to the corresponding request.
    Hardcoded variables are gone hurrayyy!!!!

--- PDMusic/Client/src/main/java/sample/controllers/tabs/musicsTab/AddMusicController.java
+++ PDMusic/Client/src/main/java/sample/controllers/tabs/musicsTab/AddMusicController.java
@@ -1,25 +1,83 @@
 package sample.controllers.tabs.musicsTab;
 
+import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import org.json.JSONObject;
 import sample.controllers.ScreenController;
 import sample.controllers.tabs.TabCommunication;
 
-import java.io.IOException;
 
 public class AddMusicController extends TabCommunication {
-    private String gen = "Pop Rock", artist = "Editors", name = "Munich", album = "Munich", year = "2005";
+
+    @FXML
+    private JFXTextField musicNameField, authorField, albumField, yearField, durationField, genreField;
+
+    private boolean isMusicSelected = false;
+
+    private String musicName, author, album, genre;
+    private int year, duration;
+
+    private boolean getFieldValues() {
+        if (!isMusicSelected) {
+            return false;
+        }
+
+        musicName = musicNameField.getText();
+        author = authorField.getText();
+        album = albumField.getText();
+        genre = genreField.getText();
+
+        try {
+            year = Integer.parseInt(yearField.getText());
+            duration = Integer.parseInt(durationField.getText());
+        } catch (NumberFormatException e) {
+            System.out.println("Year and duration need to be integers!");
+            return false;
+        }
+
+        if (musicName.isEmpty() || author.isEmpty() || album.isEmpty() || genre.isEmpty()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private void clearFields() {
+        musicNameField.clear();
+        authorField.clear();
+        albumField.clear();
+        yearField.clear();
+        durationField.clear();
+        genreField.clear();
+    }
+
     @FXML
     void saveMusic(ActionEvent event) {
+
+        if (!getFieldValues()) {
+            return;
+        }
+
         //TODO: Add music
+        getMainController().getCommunicationHandler().addMusic(musicName, author, album, year, duration, genre);
+
+        isMusicSelected = false;
+
+        clearFields();
 
         goBackToMusicsMenu();
     }
 
     @FXML
     void cancelMusicChanges(ActionEvent event) {
+        isMusicSelected = false;
         goBackToMusicsMenu();
+    }
+
+    @FXML
+    void selectMusic(ActionEvent event) {
+        //TODO: Select music
+        isMusicSelected = true;
     }
 
     private void goBackToMusicsMenu() {
--- PDMusic/Client/src/main/java/sample/controllers/tabs/playlistsTab/AddPlaylistController.java
+++ PDMusic/Client/src/main/java/sample/controllers/tabs/playlistsTab/AddPlaylistController.java
@@ -1,18 +1,29 @@
 package sample.controllers.tabs.playlistsTab;
 
+import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import org.json.JSONObject;
+
 import sample.controllers.ScreenController;
 import sample.controllers.tabs.TabCommunication;
 
-import java.io.IOException;
-
 public class AddPlaylistController extends TabCommunication {
-    private String username = "PDMUSIC1", name = "P1", gen = "Rock";
+
+    @FXML
+    private JFXTextField playlistNameField;
+
     @FXML
     void savePlaylist(ActionEvent event) {
+        String playlistName = playlistNameField.getText();
+
+        if (playlistName.isEmpty()) {
+            return;
+        }
+
         //TODO: Add playlist
+        getMainController().getCommunicationHandler().addPlaylist(playlistName);
+
+        playlistNameField.clear();
 
         goBackToPlaylistsMenu();
     }
