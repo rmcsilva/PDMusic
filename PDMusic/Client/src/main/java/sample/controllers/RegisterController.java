f534bd85914816bd43f6bdd44cb97d20fff53ccf
Ricardo <33632491+rmcsilva@users.noreply.github.com>
2019-11-14 13:01:40 +0000

    Added: Dynamic Register
    Name, username and password inserted will part of the request.
    The username that was registered will be automatically set on the login screen.

--- PDMusic/Client/src/main/java/sample/controllers/RegisterController.java
+++ PDMusic/Client/src/main/java/sample/controllers/RegisterController.java
@@ -1,5 +1,7 @@
 package sample.controllers;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
@@ -9,16 +11,49 @@ import java.io.IOException;
 
 public class RegisterController {
 
-    LoginController loginController;
+    private LoginController loginController;
+
+    @FXML
+    private JFXTextField nameField, usernameField;
+
+    @FXML
+    private JFXPasswordField passwordField;
+
+    private String name, username, password;
 
     public void setLoginController(LoginController loginController) { this.loginController = loginController; }
 
     private void goToLogin() { ScreenController.getInstance().activate(ScreenController.Screen.LOGIN); }
 
+    private boolean getFieldValues() {
+        name = nameField.getText();
+        username = usernameField.getText();
+        password = passwordField.getText();
+
+        if (name.isEmpty() || username.isEmpty() || password.isEmpty()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private void clearField() {
+        nameField.clear();
+        usernameField.clear();
+        passwordField.clear();
+    }
+
     @FXML
     void confirmRegistration(ActionEvent event) throws IOException, CountExceededException {
+        if (!getFieldValues()) {
+            return;
+        }
         //TODO: Send data back to login
         loginController.startCommunicationHandler();
+        loginController.getCommunicationHandler().register(name, username, password);
+        loginController.setUsernameFieldText(username);
+
+        clearField();
 
         goToLogin();
     }
