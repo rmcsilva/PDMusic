06ac4331629afd0994d367e41741eb415e13bad6
Ricardo <33632491+ricardoslyox@users.noreply.github.com>
2020-01-05 18:38:59 +0000

    Added: Communication Remote Exception
    Added remote exceptions on communication methods

--- PDMusic/Communication/src/main/java/sample/Communication.java
+++ PDMusic/Communication/src/main/java/sample/Communication.java
@@ -6,15 +6,15 @@ public interface Communication extends JSONConstants {
 
     void login(String username, String password) throws IOException;
     void register(String name, String username,String password) throws IOException;
-    void addMusic(String name, String author, String album, int year, int duration, String genre);
-    void editMusic(String musicToEdit, String name, String author, String album, int year, int duration, String genre);
-    void removeMusic(String musicToRemove);
-    void getMusic(String musicName);
-    void addPlaylist(String name);
-    void editPlaylist(String playlistToEdit, String name);
-    void removePlaylist(String playlistToRemove);
-    void addMusicToPlaylist(String musicName, String playlistName);
-    void removeMusicFromPlaylist(String musicToRemove, String playlistName);
-    void logout();
+    void addMusic(String name, String author, String album, int year, int duration, String genre) throws IOException;
+    void editMusic(String musicToEdit, String name, String author, String album, int year, int duration, String genre) throws IOException;
+    void removeMusic(String musicToRemove) throws IOException;
+    void getMusic(String musicName) throws IOException;
+    void addPlaylist(String name) throws IOException;
+    void editPlaylist(String playlistToEdit, String name) throws IOException;
+    void removePlaylist(String playlistToRemove) throws IOException;
+    void addMusicToPlaylist(String musicName, String playlistName) throws IOException;
+    void removeMusicFromPlaylist(String musicToRemove, String playlistName) throws IOException;
+    void logout() throws IOException;
 
 }
